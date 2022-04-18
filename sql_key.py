af9d8a24c564522938bcbe2d9429e18777fe4eed
Mel <meljaynethomas@gmail.com>
2022-04-18 20:04:16 +0100

    amended port number and created database - unable to get code working

--- sql_key.py
+++ sql_key.py
@@ -1,3 +1,3 @@
 from application import app
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://food:passWORD@localhost/EFFP"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://food:passWORD@localhost/EFFP";
