945efeeb5d1c7dcec3af29fe86968dce48cd1519
Mel <meljaynethomas@gmail.com>
2022-04-23 17:56:10 +0100

    combined register and login link. made full navbar appear when user is logged in. added some styling to the forms

--- application/routes.py
+++ application/routes.py
@@ -79,7 +79,7 @@ def login():
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('home'))
+            return redirect(next_page) if next_page else redirect(url_for('user_account'))
         else:
             flash('Login unsuccessful. Please check email and password')
     return render_template('login.html', title='Login', form=form)
